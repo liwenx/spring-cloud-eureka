334ec5a13690c981739b51511b4b9983b5c2dfac
黎文兴 <liwxd@yonyou.com>
2018-04-08 22:21:54 +0800

    创建工程

---
+++ spring-cloud-eureka-web/src/main/java/com/sc/web/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.sc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liwenxing
+ * @date 2018/1/17 15:40
+ */
+
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
